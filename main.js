daad087556544ed64b905f7ec5aafefe9358c17b
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-07-09 17:10:32 -0500

    Settings Saving & Loading
    Added the ability to save and load settings for the app into a configuration file.

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const path = require('path')
+const fs = require("fs");
+const async = require("async");
 const { app, BrowserWindow } = require('electron')
+
+let settings = null;
 
 function createWindow () {
 	const win = new BrowserWindow({
@@ -13,6 +17,47 @@ function createWindow () {
 	win.setMenu(null);
 	win.resizable = false;
 	win.loadFile('settings.html');
+	
+	async.series([
+		function(callback) {
+			loadSettings(callback);
+		}, function(callback) {
+			console.log(JSON.stringify(settings));
+			callback(null, "two");
+		}], 
+	);
+}
+
+function saveSettings () {
+	fs.writeFile("config.json", JSON.stringify(settings), (err) => {
+		if(err) 
+			console.log(err);
+		else
+			console.log("Settings saved")
+	});
+}
+
+function loadSettings (callback) {
+	fs.readFile("config.json", function(err, buf) {
+		if (err)
+			console.log(err);
+		else{
+			settings = JSON.parse(buf);
+			callback(null, "one");
+		}
+	})
+}
+
+function saveDefaultSettings () {
+	fs.writeFile("config.json", JSON.stringify(
+		{language_1: "JP", 
+		 language_2: "EN", 
+		 translation_key: "Ctrl+;", 
+		 timer_interval: 5, 
+		 scroll_translate: false, 
+		 timer_translate: true}), 
+		(err) => {if(err) console.log(err); console.log("Settings saved")
+	});
 }
 
 app.whenReady().then(() => {
