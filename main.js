cdf540ecbb41b38f02e53a6f4d6cd884b1c997bf
Pyristix <61607183+Pyristix@users.noreply.github.com>
2021-07-11 17:22:05 -0500

    Initial Settings & Timer Interval Input Validation
    Changes settings based on changes to HTML input, loads initial settings on startup, and validates the input of the timer interval setting. Also prevents text highlighting.

--- main.js
+++ main.js
@@ -25,48 +25,25 @@ function createWindow () {
 			loadSettings(callback);
 		}, function(callback) {
 			console.log(JSON.stringify(settings));
+			win.webContents.on('did-finish-load', () => {win.webContents.send("initial_settings", settings);});
 			callback(null, "two");
 		}], 
 	);
 	
-	ipcMain.on('language_1_selected', function (event, arg){
-		console.log(arg);
-	})
-	
-	ipcMain.on('language_2_selected', function (event, arg){
-		console.log(arg);
-	})
-	
-	ipcMain.on('translation_key_selected', function (event, arg){
-		console.log(arg);
-	})
-	
-	ipcMain.on('timer_interval_selected', function (event, arg){
-		console.log(arg);
-	})
-	
-	ipcMain.on('scroll_translate_input_changed', function (event, arg){
-		console.log(arg);
-	})
-	
-	ipcMain.on('time_translate_input_changed', function (event, arg){
-		console.log(arg);
-	})
-	
-	ipcMain.on('settings_submitted', function (event, arg){
-		console.log("Submitted");
-	})
+	setIpcListeners();
 }
 
+//Saves settings to config.json
 function saveSettings () {
 	fs.writeFile("config.json", JSON.stringify(settings), (err) => {
 		if(err) 
 			console.log(err);
 		else
-			console.log("Settings saved")
+			console.log("Settings saved");
 	});
 }
 
+//Loads settings from config.json into settings object
 function loadSettings (callback) {
 	fs.readFile("config.json", function(err, buf) {
 		if (err)
@@ -78,33 +55,65 @@ function loadSettings (callback) {
 	})
 }
 
+//Saves default settings to config.json
 function saveDefaultSettings () {
 	fs.writeFile("config.json", JSON.stringify(
-		{language_1: "JP", 
+		{language_1: "JA", 
 		 language_2: "EN", 
 		 translation_key: "Ctrl+;", 
 		 timer_interval: 5, 
 		 scroll_translate: false, 
 		 timer_translate: true}), 
-		(err) => {if(err) console.log(err); console.log("Settings saved")
+		(err) => {if(err) console.log(err); console.log("Settings saved");}
+	);
+}
+
+//Sets ipc event listeners to listen for changes to settings and change settings object appropriately
+function setIpcListeners () {
+	ipcMain.on('language_1_selected', function (event, arg){
+		console.log(arg);
+		settings.language_1 = arg;
+	});
+	
+	ipcMain.on('language_2_selected', function (event, arg){
+		console.log(arg);
+		settings.language_2 = arg;
+	});
+	
+	ipcMain.on('translation_key_selected', function (event, arg){
+		console.log(arg);
+		settings.translation_key = arg;
+	});
+	
+	ipcMain.on('timer_interval_selected', function (event, arg){
+		console.log(arg);
+		settings.timer_interval = arg;
+	});
+	
+	ipcMain.on('scroll_translate_input_changed', function (event, arg){
+		console.log(arg);
+		settings.scroll_translate = arg;
+	});
+	
+	ipcMain.on('time_translate_input_changed', function (event, arg){
+		console.log(arg);
+		settings.timer_translate = arg;
+	});
+	
+	ipcMain.on('settings_submitted', function (event, arg){
+		saveSettings();
+		console.log("Submitted");
+		console.log(JSON.stringify(settings));
 	});
 }
 
+//Creates window when the app is ready
 app.whenReady().then(() => {
-  createWindow()
+	createWindow();
 })
 
+//Closes window when all windows are closed
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') 
+		app.quit();
 })
-
-
-if (process.platform === 'darwin'){
-	app.whenReady().then(() => {
-	  createWindow()
-
-	  app.on('activate', function () {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow()
-	  })
-	})
-}
--- resources/javascript/settings_listeners.js
+++ resources/javascript/settings_listeners.js
@@ -1,4 +1,4 @@
-const ipc = require('electron').ipcRenderer;
+const ipcRenderer = require('electron').ipcRenderer;
 
 const language_1_input = document.getElementById('language_1');
 const language_2_input = document.getElementById('language_2');
@@ -8,31 +8,50 @@ const scroll_translate_input = document.getElementById('scroll_translate');
 const timer_translate_input = document.getElementById('timer_translate');
 const submit_button = document.getElementById('submit');
 
+//Sets initial settings on the html page upon startup
+ipcRenderer.on("initial_settings", function(event, arg) {
+	language_1_input.value = arg.language_1;
+	language_2_input.value = arg.language_2;
+	translation_key_input.value = arg.translation_key;
+	automatic_timer_input.value = arg.timer_interval;
+	scroll_translate_input.checked = arg.scroll_translate;
+	timer_translate_input.checked = arg.timer_translate;
+});
 
+//Sends events to main process when settings are changed
 language_1_input.addEventListener('input', () => {
-	ipc.send('language_1_selected', language_1_input.value)
+	ipcRenderer.send('language_1_selected', language_1_input.value)
 })
 
 language_2_input.addEventListener('input', () => {
-	ipc.send('language_2_selected', language_2_input.value)
+	ipcRenderer.send('language_2_selected', language_2_input.value)
 })
 
 translation_key_input.addEventListener('input', () => {
-	ipc.send('translation_key_selected', translation_key.value)
+	ipcRenderer.send('translation_key_selected', translation_key.value)
 })
 
 automatic_timer_input.addEventListener('input', () => {
-	ipc.send('timer_interval_selected', automatic_timer_input.value)
+	let validated_timer_value = automatic_timer_input.value;
+	if(validated_timer_value < 5){
+		validated_timer_value = 5;
+		automatic_timer_input.value = 5;
+	} else if (validated_timer_value > 9999) {
+		validated_timer_value = 9999;
+		automatic_timer_input.value = 9999;
+	}
+	
+	ipcRenderer.send('timer_interval_selected', validated_timer_value)
 })
 
 scroll_translate_input.addEventListener('input', () => {
-	ipc.send('scroll_translate_input_changed', scroll_translate_input.checked);
+	ipcRenderer.send('scroll_translate_input_changed', scroll_translate_input.checked);
 })
 
 timer_translate_input.addEventListener('input', () => {
-	ipc.send('time_translate_input_changed', timer_translate_input.checked);
+	ipcRenderer.send('time_translate_input_changed', timer_translate_input.checked);
 })
 
 submit_button.addEventListener('click', () => {
-	ipc.send('settings_submitted', "test");
+	ipcRenderer.send('settings_submitted', "test");
 })
